595486ca48a4aee2288fc9226c4cf8bd06eca3a7
Paul Möhnen <uhjqa@student.kit.edu>
2023-01-02 16:05:46 +0100

    added some classes

---
+++ flutter_frontend_recipes/lib/content_examples/recipe-examples.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_frontend_recipes/types/ingredient.dart';
+import 'package:flutter_frontend_recipes/types/recipe.dart';
+
+class RecipeExamples {
+
+  
+
+
+  static RARecipe testRecipe1 = RARecipe(
+    picture: 'assets/example_pictures/hamburger.jpg',
+    title: "Burger mit Basilikum",
+    description:
+        "Ein gewöhnlicher Burger mit dem gewissen etwas. Basilikum verschönert nicht nur, sondern gibt dem Burger seinen ganz eigenen Character",
+    ingredients: [],
+    manual: [],
+  );
+}
