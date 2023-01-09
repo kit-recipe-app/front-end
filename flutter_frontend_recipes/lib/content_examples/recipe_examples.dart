f30931ad3d135f74d4d2ec572e7d4afb3bb19a7b
Benjamin Weng <benjamingweng@gmx.de>
2023-01-09 20:52:37 +0100

    updated main page

--- flutter_frontend_recipes/lib/content_examples/recipe_examples.dart
+++ flutter_frontend_recipes/lib/content_examples/recipe_examples.dart
@@ -4,7 +4,7 @@ import 'package:flutter_frontend_recipes/types/recipe.dart';
 class RecipeExamples {
   static RARecipe testRecipe1 = RARecipe(
     picture: 'assets/example_pictures/hamburger.jpg',
-    title: "Burger mit Basilikum und heheh",
+    title: "Burger mit Basilikum und Pommes",
     description:
         "Ein gewöhnlicher Burger mit dem gewissen etwas. Basilikum verschönert nicht nur, sondern gibt dem Burger seinen ganz eigenen Character",
     ingredients: [
