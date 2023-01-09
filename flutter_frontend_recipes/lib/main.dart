4fc7202696389090ef54297cdd8fb8f5ee91abe4
Paul Möhnen <uhjqa@student.kit.edu>
2023-01-09 18:21:46 +0100

    added firebase connection (hopefully)

--- flutter_frontend_recipes/lib/main.dart
+++ flutter_frontend_recipes/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend_recipes/recipe_app.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
